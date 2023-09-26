a724f5d204acb61addbedead943d071a9e104b00
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2023-09-26 12:54:01 +0300

    build react project for exercises

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { ApolloClient, ApolloProvider, InMemoryCache, gql } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+})
+
+const query = gql`
+  query {
+    allAuthors {
+      name
+    }
+  }
+`
+
+client.query({ query })
+  .then((response) => {
+    console.log(response.data)
+  })
+
+
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  )
