4623505937fd861c0a336c84b781ef3adeb9d0fd
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2023-10-02 15:24:37 +0300

    add user functionality

--- server/index.js
+++ server/index.js
@@ -2,12 +2,12 @@ const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
 const { v1: uuid } = require('uuid')
 const { GraphQLError } = require('graphql')
-
+const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
 const Book = require('./models/book')
 const Author= require('./models/author')
-
+const User = require('./models/user')
 require('dotenv').config()
 
 const MONGODB_URI = process.env.MONGODB_URI
@@ -24,6 +24,16 @@ mongoose.connect(MONGODB_URI)
 
 
 const typeDefs = `
+  type User {
+    username: String!
+    favoriteGenre: String!
+    id: ID!
+  }
+
+  type Token {
+    value: String!
+  }
+
   type Book {
     title: String!
     published: Int!
@@ -53,6 +63,14 @@ const typeDefs = `
       name: String
       setBornTo: Int
     ): Author
+    createUser(
+      username: String!
+      favoriteGenre: String!
+    ): User
+    login(
+      username: String!
+      password: String!
+    ): Token
   }
 
   type Query {
@@ -60,6 +78,7 @@ const typeDefs = `
     authorCount: Int!,
     allBooks(author: String,genre: String): [Book!]!
     allAuthors: [Author!]!
+    me:User
   }  
 `
 
@@ -99,8 +118,41 @@ const resolvers = {
       author.born = args.setBornTo
       const savedAuthor =await author.save()
       return author
-    }
+    },
+    createUser: async (root, args) => {
+      const user = new User({ username: args.username, favoriteGenre: args.favoriteGenre })
+  
+      return user.save()
+        .catch(error => {
+          throw new GraphQLError('Creating the user failed', {
+            extensions: {
+              code: 'BAD_USER_INPUT',
+              invalidArgs: args.name,
+              error
+            }
+          })
+        })
+    },
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username })
+  
+      if ( !user || args.password !== 'secret' ) {
+        throw new GraphQLError('wrong credentials', {
+          extensions: {
+            code: 'BAD_USER_INPUT'
+          }
+        })        
+      }
+  
+      const userForToken = {
+        username: user.username,
+        id: user._id,
+      }
+  
+      return { value: jwt.sign(userForToken, process.env.JWT_SECRET) }
+    },
   },
+    
   Query: {
     
     bookCount: async (root, args) => {
@@ -127,7 +179,7 @@ const resolvers = {
       }
 
       return await Book.find(query);
-  },
+    },
 
     allAuthors: async (root, args) => {
       return Author.find({})
