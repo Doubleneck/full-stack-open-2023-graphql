c649dec66828bed837f4b625ccbd82e625106fff
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2023-10-02 15:43:33 +0300

    add context

--- server/index.js
+++ server/index.js
@@ -154,7 +154,9 @@ const resolvers = {
   },
     
   Query: {
-    
+    me: (root, args, context) => {
+      return context.currentUser
+    },
     bookCount: async (root, args) => {
       if (!args.author) {
         const bookCount = await Book.countDocuments({})
@@ -213,6 +215,18 @@ const server = new ApolloServer({
 
 startStandaloneServer(server, {
   listen: { port: 4000 },
+  context: async ({ req, res }) => {
+    const auth = req ? req.headers.authorization : null
+    if (auth && auth.startsWith('Bearer ')) {
+      const decodedToken = jwt.verify(
+        auth.substring(7), process.env.JWT_SECRET
+      )
+      const currentUser = await User
+        .findById(decodedToken.id)
+      console.log(currentUser)
+      return { currentUser }
+    }
+  },
 }).then(({ url }) => {
   console.log(`Server ready at ${url}`)
 })
